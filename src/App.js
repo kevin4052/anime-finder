6f4c535b8ede671deb9507e7d0c8dcf80ce8a42d
kevin4052 <kevin.hernandez001@gmail.com>
2020-10-09 14:18:43 -0400

    component mount api testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    anime: {}
+  }
+
+  componentDidMount = () => {
+    const urlBase = 'https://api.jikan.moe/v3/';
+    axios
+      .get(`${urlBase}anime/1`)
+      .then(async response => {
+
+        console.log(response.data);
+
+        await this.setState({
+          anime: response.data
+        });
+
+        console.log(this.state.anime.genres)
+
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.anime.title}</h1>
+        <p>{this.state.anime.synopsis}</p>
+        <ul>
+          {
+            this.state.anime.genres?.map((genre) => <li key={genre.mal_id}>{genre.name}</li> )
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
